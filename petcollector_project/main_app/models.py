26c6a7effabdc64eeef5eb31c1f67e3db65da641
ABHI <am@MacBook-Pro.local>
2021-03-12 19:01:17 -0800

    adds a new model Feeding

--- petcollector_project/main_app/models.py
+++ petcollector_project/main_app/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+# Choices to be added on feeding
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 class Pet(models.Model):
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=100)
@@ -12,4 +19,18 @@ class Pet(models.Model):
     def __str__(self):
         return self.name
         
+class Feeding(models.Model):
+    date = models.DateField("Feeding Date")
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+        )
+    
+    # Create a Pet id FK
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+
     
