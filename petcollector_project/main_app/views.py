f999e0aae19badab2f81936486d5a9c09f93833e
ABHI <am@MacBook-Pro.local>
2021-03-12 18:32:57 -0800

    adds models Pet and changes made in views to display petts in View all mmy pets page and migrations were done

--- petcollector_project/main_app/views.py
+++ petcollector_project/main_app/views.py
@@ -1,19 +1,6 @@
 from django.shortcuts import render
+from .models import Pet
 
-# Create your views here.
-class Pet:
-    def __init__(self, name, species, breed, description, age):
-        self.name = name
-        self.species = species
-        self.breed = breed
-        self.description = description
-        self.age = age
-    
-pets = [
-    Pet('Lolo', "cat", 'tabby', 'foul little demon', 3),
-    Pet('Sachi', "cat", 'tortoise shell', 'diluted tortoise shell', 0),
-    Pet('Raven', "cat", 'black tripod', '3 legged cat', 4)
-]
 def home(request):
     return render(request, 'home.html')
 
@@ -21,4 +8,5 @@ def about(request):
     return render(request, "about.html")
 
 def pets_index(request):
+    pets = Pet.objects.all()
     return render(request, "pets/index.html" , {"pets" : pets})
