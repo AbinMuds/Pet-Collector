0ed4ad08a79e940b15a59e6ee711d1afc7af5f2d
ABHI <am@MacBook-Pro.local>
2021-03-12 17:20:00 -0800

    adds a views and urls for the view_my_pets, also edited home.html and some random datas for the pets is given temporarily

--- petcollector_project/main_app/views.py
+++ petcollector_project/main_app/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+class Pet:
+    def __init__(self, name, species, breed, description, age):
+        self.name = name
+        self.species = species
+        self.breed = breed
+        self.description = description
+        self.age = age
+    
+pets = [
+    Pet('Lolo', "cat", 'tabby', 'foul little demon', 3),
+    Pet('Sachi', "cat", 'tortoise shell', 'diluted tortoise shell', 0),
+    Pet('Raven', "cat", 'black tripod', '3 legged cat', 4)
+]
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, "about.html")
+
+def pets_index(request):
+    return render(request, "pets/index.html" , {"pets" : pets})
